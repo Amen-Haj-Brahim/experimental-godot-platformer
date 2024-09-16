95618771be64e98891ac11a7cdf42b0cca9c6a6f
Amen-Haj-Brahim <amanallahhajbrahim2003@gmail.com>
2024-09-16 18:10:25 +0100

    added wall jump

--- scripts/playermovement.gd
+++ scripts/playermovement.gd
@@ -5,6 +5,7 @@ const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 @export_range(0,1) var acceleration=0.1
 @export_range(0,1) var deceleration=0.1
+@export_range(0,1) var decelerate_jump=0.5
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta):
@@ -13,9 +14,11 @@ func _physics_process(delta):
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or is_on_wall()):
 		velocity.y = JUMP_VELOCITY
-
+	if Input.is_action_just_released("jump") and velocity.y<0:
+		velocity.y*=decelerate_jump
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction  = Input.get_axis("move_left", "move_right")
